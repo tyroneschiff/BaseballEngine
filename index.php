d015c8f94d49c51753aa61e02e25661cc1d65caa
Tyrone Schiff <tyroneschiff@gmail.com>
2012-02-20 02:22:02 -0500

    Updated as of 2/20/2012

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<!-- Chosen Styling -->
 		<link rel="stylesheet" href="chosen/chosen/chosen.css">
+		<link rel="stylesheet" href="selections.css">
 
 		<!-- Bootstrap Styling -->
 		<link rel="stylesheet" href="bootstrap/docs/assets/css/bootstrap.css">
@@ -36,6 +37,22 @@
 			.chzn-container {
 				top:1px;
 			}
+			.navbar .nav > li > a {
+				color:white;
+			}
+			.dropdown-menu {
+				min-width:260px;
+				max-width:320px;
+			}
+			.dropdown-definition {
+				list-style-type:none;
+				color:#999;
+			}
+			@media (max-width: 980px) {
+				.navbar .brand {
+					margin:10px 0 0 5px;
+				}
+			}
 		</style>
 		<title>The Baseball Engine</title>
 	</head>
@@ -46,17 +63,40 @@
 		<div class="navbar-inner">
 			<div class="container" style="width:auto;">
 				<a class="brand" href="#">The Baseball Engine</a>
-				<form class="navbar-search pull-left">
-					<select data-placeholder="Select your batters..." class="chzn-select span6" multiple>
-						<? foreach ( $batters as $id => $player_names ): ?>
-						<option><?= $player_names; ?></option>
-						<? endforeach; ?>
-					</select>
-				</form>
+				<ul class="nav pull-left">
+					<li>
+						<form class="navbar-search">
+							<select data-placeholder="Select your batters..." class="chzn-select span6" multiple>
+								<? foreach ( $batters as $id => $player_names ): ?>
+								<option <? if ( in_array( $player_names, $topBEscores ) ) { echo "selected=selected"; } ?>><?= $player_names; ?></option>
+								<? endforeach; ?>
+							</select>
+						</form>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<span style="font-size:18px;">BE Score</span>
+							<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu">
+							<li>
+								<a href="#">BE Score</a>
+								<ul class="dropdown-definition">
+									<li>
+										The Baseball Engine's method for ranking players
+									</li>
+								</ul>
+							</li>
+							<li>
+								<a href="#">Average</a>
+							</li>
+						</ul>
+					</li><!-- end dropdown -->
+				</ul><!-- end nav pull-left -->
 				<ul class="nav pull-right">
 					<li class="divider-vertical"></li>
 					<li><a href="#">Go Pro</a></li>
-				</ul>
+				</ul><!-- end nav pull-right -->
 			</div><!-- End container -->
 		</div><!-- End navbar-inner -->
 	</div><!-- End navbar -->
