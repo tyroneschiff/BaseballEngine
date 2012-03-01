ea2644481bc15fbf0e9b002be230354aafac0534
Tyrone Schiff <tyroneschiff@gmail.com>
2012-03-01 02:07:02 -0500

    Updated as of 3/1/2012

--- changestat.js
+++ changestat.js
@@ -1,19 +1,9 @@
 $(document).ready( function(){
-	var dropdown = $('div.btn-group');
-
 	$('ul.dropdown-menu li a:not(.pro-stat)').on('click',function(){
-		var statistic = $(this).text();
+		var statistic = $(this).text(),
+				type = $('div.btn-group button.btn.active').data('pos'); 
 		$('a.btn.dropdown-toggle span.current-stat').text(statistic);
+		getPlayers( statistic, type );
 		drawGraph();
 	});
-
-	/*
-	$('a.btn.dropdown-toggle').on('mouseover',function(){
-		dropdown.addClass('open');
-	});
-
-	$('ul.dropdown-menu').on('mouseleave',function(){
-		dropdown.removeClass('open');
-	});
-	*/
 });
