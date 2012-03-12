1698c53717336c729c839ac9dbaf1c7214b4c370
Tyrone Schiff <tyroneschiff@gmail.com>
2012-03-12 01:32:20 -0400

    Updated login

--- login/index.php
+++ login/index.php
@@ -9,52 +9,31 @@
 		
 		<!-- Favicon -->
 		<link href="/images/favicon.ico" rel="icon" type="image/x-icon" />
-		<style>
-			.modal {
-				left:58%;
-				width:360px;
-			}
-			div.modal-header h3 {
-				text-align:center;
-				font-family: 'Acme', sans-serif;
-				font-size:24px;
-			}
-			div.well {
-				margin-bottom:0;
-			}
-			div.well form {
-				margin:0;
-			}
-			div.well label {
-				font-weight:bold;
-			}
-			div.well input {
-				height:27px;
-				width:260px;
-			}
-			div.well i.icon-exclamation-sign {
-				color:red;
-				position:relative;
-				top:-4px;
-				left:5px;
-			}
-			div.modal-footer a.forgot, div.modal-footer a.login {
-				float:left;
-				padding:5px;
-				cursor:pointer;
-			}
-			div.modal-footer a.forgot:hover, div.modal-footer a.login:hover {
-				text-decoration:underline;
-			}
-		</style>
+
+		<!-- Page Specific Styling -->
+		<link rel="stylesheet" href="/login/css/style.css">
 		<title>The Baseball Engine - Login</title>
 	</head>
 <body>
 	<div class="modal" id="login">
 		<div class="modal-header">
-			<h3>The Baseball Engine</h3>
+			<a href="/"><h3>The Baseball Engine</h3></a>
 		</div>
 		<div class="modal-body">
+			<?
+        if ( array_key_exists( 'e', $_GET ) ) echo "
+        <div class='alert alert-error'>
+          <span>E-mail address and/or password were not found. Please try again.</span>
+        </div>";
+				if ( array_key_exists( 'd', $_GET ) ) echo "
+        <div class='alert alert-info'>
+          <span>That e-mail address has already been registered. Please log in or reset password.</span>
+        </div>";
+        if ( array_key_exists( 'c', $_GET ) ) echo "
+        <div class='alert alert-success'>
+          <span>A password reset e-mail has been sent. Please check your e-mail.</span>
+        </div>";
+      ?>
 			<div class="well"><!-- Begin well -->
 				<form action="checkLogin.php" method="post">
 					<label>E-mail Address</label>
@@ -64,12 +43,6 @@
 					<input type="password" name="passwordPro" />
 					<? if ( array_key_exists( 'e', $_GET ) ) echo "<i class='icon-exclamation-sign'></i>"; ?>
 			</div>
-			<? 
-				if ( array_key_exists( 'e', $_GET ) ) echo "
-				<div style='margin-top:10px;text-align:center;font-size:11px;'>
-					<span style='color:red;margin-top:20px;'>Email address and password were not found. Please try again.</span>
-				</div>";
-			?>
 		</div>
 		<div class="modal-footer">
 			<a class="forgot">Forgot Password</a>
@@ -84,35 +57,21 @@
 		</div>
 		<div class="modal-body">
 			<div class="well"><!-- Begin well -->
-				<label>E-mail Address</label>
-				<input type="text" name="email" class="forgot-password" autofocus/>
+				<form action="/login/forgotPassword.php" method="post">
+					<label>E-mail Address</label>
+					<input type="text" name="email" class="forgot-password" autofocus/>
 			</div>
 		</div>
 		<div class="modal-footer">
 			<a class="login">Log In</a>
-			<button type="submit" class="btn btn-inverse">Send Forgot Password E-mail</button>
+			<button type="submit" class="btn btn-inverse">Send Reset Password E-mail</button>
+				</form>
 		</div>
 	</div>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.8.1/jquery.validate.min.js"></script>
 <script type="text/javascript" src="/bootstrap/docs/assets/js/google-code-prettify/prettify.js"></script>
 <script type="text/javascript" src="/bootstrap/docs/assets/js/bootstrap-modal.js"></script>
-<script>
-	$('#login').modal({
-		show: true,
-		backdrop: 'static',
-		keyboard: false
-	});
-
-	$('.forgot').on('click',function(){
-		$('#login').hide();
-		$('#forgot').show();
-	});
-
-	$('.login').on('click',function(){
-		$('#login').show();
-		$('#forgot').hide();
-	});
-</script>
+<script type="text/javascript" src="/login/js/script.js"></script>
 </body>
 </html>
